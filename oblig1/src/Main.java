249e1479dc6f4512d72925ad9d81186308eb0ba4
Alexander Fleischer <alexander@xal.no>
2018-01-29 15:26:55 +0100

    Cleaned up Main. Removed part 1 Main.

--- oblig1/src/Main.java
+++ oblig1/src/Main.java
@@ -1,5 +1,7 @@
 class Main {
     public static void main (String[] args) {
+        // Hvis ett kommandolinje-argument, gjør del 2 av oblig.
+        if (args.length == 1) {
         // Opprett nytt Regneklynge-objekt ved navn abel.
         Regneklynge abel = new Regneklynge(args[0]);
 
@@ -13,5 +15,36 @@ class Main {
 
         System.out.println("Antall prosessorer: " + abel.antProsessorer());
         System.out.println("Antall racks: " + abel.antRacks());
+        }
+        // Hvis det ikke finnes kommandolinje-arg, gjør del 1 av oblig.
+        else if (args.length == 0) {
+            // Opprett nytt Regneklynge-objekt ved navn abel.
+            Regneklynge abel = new Regneklynge(12);
+
+            // Sett inn 650 noder med 64 GB minne og 1 prosessor.
+            for (int i = 0; i < 650; i++) {
+                abel.settInnNode(new Node(64, 1));
+            }
+
+            // Sett inn 16 noder med 1024 GB minne og 2 prosessorer.
+            for (int i = 0; i < 16; i++) {
+                abel.settInnNode(new Node(1024, 2));
+            }
+
+            System.out.println("\nNoder med minst 32 GB: " 
+                                + abel.noderMedNokMinne(32));
+            System.out.println("Noder med minst 64 GB: " 
+                                + abel.noderMedNokMinne(64));
+            System.out.println("Noder med minst 128 GB: " 
+                                + abel.noderMedNokMinne(128) 
+                                + "\n");
+
+            System.out.println("Antall prosessorer: " + abel.antProsessorer());
+            System.out.println("Antall racks: " + abel.antRacks());
+
+        }
+        else {
+            System.out.println("Maks ett filnavn som argument.");
+        }
     }
 }
