406f5df12162d5f7b6129b5f3460959f85f09b59
Alexander Fleischer <alexander@xal.no>
2018-03-06 16:53:04 +0100

    Started oblig 3, in1010

---
+++ oblig3/src/Lenkeliste.java
@@ -0,0 +1,51 @@
+public class Lenkeliste<T> implements Liste<T>
+{
+    private T adsf;
+
+    public Lenkeliste()
+    {
+
+    }
+
+
+    public T fjern()
+    {
+        return adsf;
+    }
+
+    
+    public T fjern(int pos)
+    {
+        return adsf;
+    }
+
+
+    public int stoerrelse()
+    {
+        return 2;
+    }
+
+
+    public void leggTil(int pos, T x)
+    {
+
+    }
+
+
+    public void leggTil(T x)
+    {
+
+    }
+
+
+    public void sett(int pos, T x)
+    {
+
+    }
+
+
+    public T hent(int pos)
+    {
+        return adsf;
+    }
+}
